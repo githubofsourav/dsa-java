468042d9fd1a17318ac70c844a6019259cd632ef
Sourav Das <77064107+githubofsourav@users.noreply.github.com>
2024-08-25 09:21:14 +0000

    re 1. introduction to recursion

---
+++ recursion/Recursion_01.java
@@ -0,0 +1,15 @@
+
+class Recursion_01 {
+    private static int count = 0;
+    
+    private static void fun() {
+        if(count == 3)
+            return;
+        System.out.println(count);
+        count ++;
+        fun();
+    }
+    public static void main(String[] args) {
+        fun();
+    }
+}
